33e4a24e9df3863a7898a465687c7f9471b9de61
Yudi Matsuguma <yudimatsuguma@gmail.com>
2023-09-20 20:47:56 -0300

    initial files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    declarations: [AppComponent],
   }));
 
   it('should create the app', () => {
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'chat-web'`, () => {
+  it('should have as title \'chat-web\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('chat-web');
